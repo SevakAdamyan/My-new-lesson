a77b7fe9f37ddca078a31bbf94910284464f15bf
Sevak Adamyan <102740109+SevakAdamyan@users.noreply.github.com>
2022-04-05 17:21:37 +0400

    "My second homework"

---
+++ Lesson.2.main.py
@@ -0,0 +1,17 @@
+# write a program that will take a five-digit number ,
+# and will insert each digit in the given number in one list in order from beginning to end.
+
+# write a program that will take a five-digit number and will insert each digit in the
+# given number in one list in order from beginning to end.
+
+my_list = [15, 345, 15, 19, -18, 78, 29, 89, 39, 100]
+my_list.sort(reverse=True)
+print(my_list)
+oder_list=my_list[:3]
+print(oder_list)
+tuple1 = tuple('50764')
+print(tuple1)
+my_list1 =list(tuple1)
+print(my_list1)
+my_list1.sort(reverse=True)
+print(my_list1)
