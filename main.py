8d95a82f9379691c25728d2459097393dc3bb8f4
Sevak Adamyan <102740109+SevakAdamyan@users.noreply.github.com>
2022-04-05 19:42:17 +0400

    My_homework about if,else

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-a = "44"
-b = "15"
-c= "30"
-if "10"<c:
-    print("a")
-elif "10">c:
-    print("b")
+#Input a year from keypboard,identify it as leap or not leap year.
+
+
+year = int(input("any year"))
+
+if year / 4 == 0 and year / 100 != 0:
+    if year / 400 == 0:
+        print('leap year')
+else:
+    print('isnt leap year')
+
+
