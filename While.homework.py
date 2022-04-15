9f8f1e9d9403decfd14f759cb07216a5cfbcc424
Sevak Adamyan <102740109+SevakAdamyan@users.noreply.github.com>
2022-04-15 15:55:04 +0400

    Add files via upload

---
+++ While.homework.py
@@ -0,0 +1,19 @@
+#Write programme, which with keybord can input two numbers and will aggregate, will extract, will deal and will multiply
+#numbers. This programme couldn't stop and will say new numbers.Programme will stop,when you input "0".You must tell,
+# that you cann't to deal "0" and if you input "0", programme must tell wrong number.
+
+num = int(input("any number: "))
+num1 = int(input("any number: "))
+i = 1
+num4 = "+"
+num5 = "/"
+num6 = "-"
+num7 = "*"
+while i > 0:
+    if i !=0:
+     print(num5)
+     print(num6)
+     print(num7)
+     print(num4)
+     if num == 0:
+         print('new number')
