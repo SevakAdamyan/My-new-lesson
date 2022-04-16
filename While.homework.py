aa061b2622b8e75275c89e855a7691f4bdb7955e
Sevak Adamyan <102740109+SevakAdamyan@users.noreply.github.com>
2022-04-16 15:34:12 +0400

    Add files via upload

--- While.homework.py
+++ While.homework.py
@@ -2,21 +2,23 @@
 #numbers. This programme couldn't stop and will say new numbers.Programme will stop,when you input "0".You must tell,
 # that you cann't to deal "0" and if you input "0", programme must tell wrong number.
 
-num = int(input("any number: "))
-num1 = int(input("any number: "))
-num2 = input("any symbol: ")
 
-i = 0
 
-while i > 0:
-    if i !=0 and num2 == "/":
-     print(num/num1)
+while True:
+    num = int(input("any number: "))
+    num1 = int(input("any number: "))
+    num2 = input("any symbol: ")
+
+    if num2 != 0 and num2 == "/":
+       print(num / num1)
     elif num2 == "+":
-     print(num+num1)
+        print(num + num1)
     elif num2 == "-":
-      print(num-num1)
+        print(num - num1)
     elif num2 == "*":
-      print(num*num1)
+        print(num * num1)
+    elif num2 == 0:
+        break
     else:
 
         print('new number')
